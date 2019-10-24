798da98e999c08d5c44394c558f328efd56a2bfd
Jan Tabaczynski <jantabaczynski@gmail.com>
2019-10-24 01:13:52 -0700

    Redeploy

---
+++ precache-manifest.44283804e20936690c640a489fb9feb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce7da1488e42fd57d64d0f113f014614",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f611c71c23cfd1b81380",
+    "url": "/static/css/main.5fddde53.chunk.css"
+  },
+  {
+    "revision": "d434ea779c7d3906b631",
+    "url": "/static/js/2.8a47245e.chunk.js"
+  },
+  {
+    "revision": "f611c71c23cfd1b81380",
+    "url": "/static/js/main.76786cf4.chunk.js"
+  },
+  {
+    "revision": "b66205ae740ba7eeae39",
+    "url": "/static/js/runtime-main.ac76bafd.js"
+  },
+  {
+    "revision": "4693408f3061f05c110c1f64321a7c83",
+    "url": "/static/media/portrait.4693408f.jpg"
+  }
+]);

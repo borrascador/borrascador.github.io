57b3c73dbeb6dda4260f18aa886742ab7fdf3919
Jan Tabaczynski <jantabaczynski@gmail.com>
2019-10-24 01:11:34 -0700

    Remove annoying huge README and update scripts

---
+++ src/components/PoetryPage.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const PoetryPage = () => (
+  <div>
+    <div className="header">
+      <div className="user-page">
+        <h1>Jan Tabaczynski</h1>
+        <br/>
+        <p>
+          This is a good pome
+        </p>
+        <p>
+          Some poetry is good for you
+        </p>
+        <p>
+          Other times, hollow words
+        </p>
+        <p>
+          Ring mostly true
+        </p>
+      </div>
+    </div>
+    <div className="user-page">
+      <br/>
+      <br/>
+      <h3>This Thing Hurts</h3>
+      <br/>
+      <p>
+        This is a good pome
+      </p>
+      <p>
+        Some poetry is good for you
+      </p>
+      <p>
+        Other times, hollow words
+      </p>
+      <p>
+        Ring mostly true
+      </p>
+      <br/>
+      ~~~
+      <br/>
+      <br/> 
+      <h3>This Thing Hurts</h3>
+      <br/>
+      <p>
+        This is a good pome
+      </p>
+      <p>
+        Some poetry is good for you
+      </p>
+      <p>
+        Other times, hollow words
+      </p>
+      <p>
+        Ring mostly true
+      </p>
+    </div>
+  </div>
+)
+
+export default PoetryPage;
